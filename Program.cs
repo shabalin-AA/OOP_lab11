4af99acc2a73f1e8ea4af618f71f65d25967cfac
shabalin-AA <92672124+shabalin-AA@users.noreply.github.com>
2022-12-10 18:46:47 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using lab10;
+
+namespace lab11
+{
+    class Program
+    {
+        static Stopwatch sw = new Stopwatch();
+        static int n = 1000;
+        static TestCollections tc = new TestCollections(n);
+
+
+        static void Find(SchoolBook toFind)
+        {
+            sw.Start();
+            tc.TKeyCollection1.Contains(toFind.Base);
+            sw.Stop();
+            Console.WriteLine(
+                $"TKeyCollection1   (Contains):      {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            tc.StringCollection1.Contains(toFind.Base.ToString());
+            sw.Stop();
+            Console.WriteLine(
+                $"StringCollection1 (Contains):      {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            tc.TKeyCollection2.ContainsKey(toFind.Base);
+            sw.Stop();
+            Console.WriteLine(
+                $"TKeyCollection2   (ContainsKey):   {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            tc.StringCollection2.ContainsKey(toFind.Base.ToString());
+            sw.Stop();
+            Console.WriteLine(
+                $"StringCollection2 (ContainsKey):   {sw.Elapsed}");
+            sw.Reset();
+
+            sw.Start();
+            tc.StringCollection2.ContainsValue(toFind);
+            sw.Stop();
+            Console.WriteLine(
+                $"StringCollection2 (ContainsValue): {sw.Elapsed}");
+            sw.Reset();
+        }
+
+
+        static void Main()
+        {
+            Console.WriteLine("First element: \n");
+            SchoolBook b1 = tc.constructor[0];
+            Find((SchoolBook)b1.Clone());
+
+            Console.WriteLine("\n\nLast element: \n");
+            SchoolBook b2 = tc.constructor[n - 1];
+            Find((SchoolBook)b2.Clone());
+
+            Console.WriteLine("\n\nCenter element: \n");
+            SchoolBook b3 = tc.constructor[n / 2 - 1];
+            Find((SchoolBook)b3.Clone());
+
+            Console.WriteLine("\n\nNon-existing element: \n");
+            SchoolBook b4 = new SchoolBook();
+            Find((SchoolBook)b4.Clone());
+        }
+    }
+}
---
+++ TestCollections.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using lab10;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace lab11
+{
+    public class TestCollections
+    {
+        public SchoolBook[] constructor;
+
+
+        public List<Book> TKeyCollection1;
+        public List<string> StringCollection1;
+
+        public SortedDictionary<Book, SchoolBook> TKeyCollection2;
+        public SortedDictionary<string, SchoolBook> StringCollection2;
+
+
+        public TestCollections(int n)
+        {
+            TKeyCollection1 = new List<Book>(n);
+            StringCollection1 = new List<string>(n);
+            TKeyCollection2 = new SortedDictionary<Book, SchoolBook>(new PrintingComparer());
+            StringCollection2 = new SortedDictionary<string, SchoolBook>();
+
+            constructor = new SchoolBook[n];
+            for (int i = 0; i < n; i++)
+            {
+                SchoolBook sb;
+                do { sb = new SchoolBook(); }
+                while (!StringCollection2.TryAdd(sb.Base.ToString(), sb));
+
+                constructor[i] = sb;
+                TKeyCollection1.Add(sb.Base);
+                StringCollection1.Add(sb.Base.ToString());
+                TKeyCollection2.Add(sb.Base, sb);
+            }
+        }
+
+
+        public void add(SchoolBook sb)
+        {
+            TKeyCollection1.Add(sb.Base);
+            StringCollection1.Add(sb.Base.ToString());
+            TKeyCollection2.Add(sb.Base, sb);
+            StringCollection2.Add(sb.Base.ToString(), sb);
+        }
+
+
+        public void remove(SchoolBook sb)
+        {
+            TKeyCollection1.Remove(sb.Base);
+            StringCollection1.Remove(sb.Base.ToString());
+            TKeyCollection2.Remove(sb.Base);
+            StringCollection2.Remove(sb.Base.ToString());
+        }
+    }
+}
+
